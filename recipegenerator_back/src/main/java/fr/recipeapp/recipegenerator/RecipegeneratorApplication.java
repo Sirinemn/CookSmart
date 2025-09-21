677e3ce9534bbbc1f3d2762d4176a6689166e04e
Sirinemn <sirinemn@outlook.fr>
2025-09-21 20:33:19 +0200

    first commit

---
+++ recipegenerator_back/src/main/java/fr/recipeapp/recipegenerator/RecipegeneratorApplication.java
@@ -0,0 +1,13 @@
+package fr.recipeapp.recipegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipegeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipegeneratorApplication.class, args);
+	}
+
+}
---
+++ recipegenerator_back/src/test/java/fr/recipeapp/recipegenerator/RecipegeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.recipeapp.recipegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipegeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
